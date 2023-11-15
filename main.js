2aecdc1f6f726928d5fb46f7869ca6e54526162e
GiorgioMos <giorgiomosca2@gmail.com>
2023-11-15 20:37:06 +0100

    first commit, nobonus)

---
+++ main.js
@@ -0,0 +1,17 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue 🍤!'
+        }
+    }
+}).mount('#app')
+
+createApp({
+    data() {
+        return {
+            messaggio: 'viva gli ananas 🍍 '
+        }
+    }
+}).mount('#🍍')
