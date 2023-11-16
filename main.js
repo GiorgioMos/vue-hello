0694ee47a28e72f3201dea54f8d83b5d0de22f9d
GiorgioMos <giorgiomosca2@gmail.com>
2023-11-16 11:59:06 +0100

    Ho provato a fare il Bonus

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ createApp({
     data() {
         return {
             messaggio: 'viva gli ananas 🍍 '
+            // imgPath: 'https://mywowo.net/media/images/cache/dubai_img_worlds_of_adventure_01_presentazione_jpg_1200_630_cover_85.jpg'
         }
     }
 }).mount('#🍍')
